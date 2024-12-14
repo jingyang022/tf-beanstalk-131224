efd26a4fe15bde248b02a11986fb5a68b5814a6c
jingyang022 <jingyang022@yahoo.com.sg>
2024-12-14 12:14:48 +0800

    corrected the solution stack issue

--- vars.tf
+++ vars.tf
@@ -1,13 +1,13 @@
 variable "elasticapp" {
-  default = "myapplication"
+  default = "yap-python-app"
 }
 
 variable "beanstalkappenv" {
-  default = "myenvironment"
+  default = "yap-environment"
 }
 
 variable "solution_stack_name" {
-  default = "Python 3.12 running on 64bit Amazon Linux 2023"
+  default = "64bit Amazon Linux 2023 v4.3.1 running Python 3.12"
 }
 
 variable "tier" {
@@ -15,9 +15,9 @@ variable "tier" {
 }
  
 variable "vpc_id" {
-    default = "vpc-0fbc7935aa69ecd58"
+    default = "vpc-06681ebfd8bb79e72"
 }
 
 variable "public_subnets" {
-    default = ["subnet-0d114957c1ac76574", "subnet-05d0fa7f2b4d972be", "subnet-0341a57e5a9fbd8c8"]
+    default = ["subnet-09ed4360baa155cd7", "subnet-09e0af039098595d7", "subnet-0308b26a65a44f935"]
 }
